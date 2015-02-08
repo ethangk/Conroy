82bb8dca23173d99c4ab061892e71011ae636b82
Ethan Katzenberg <ethankatzenberg@Ethans-MacBook-Pro.local>
2015-02-08 18:20:04 +0000

    Added the server code to the root, added README

---
+++ README.md
@@ -0,0 +1,2 @@
+A method for performing distributed computing through the browser. This was made in 24 hours in a hackathon for the EF selection weekend, and as such, the code is pretty messy. Hopefully I'll get around to cleaning up it up at some point.<br /><br />
+
---
+++ public/JobRunner.js
@@ -0,0 +1,51 @@
+var socket = io('http://localhost:12345');
+
+console.log("running job runner");
+
+var runningJobs = {};
+
+socket.on('connect', function(){
+  console.log("connection received");
+});
+
+socket.on('initTask', function(task) {
+  console.log('received', task);
+  runningJobs[task.taskID] = task; // new task to work on
+});
+
+socket.on('taskPiece', function(taskPiece) {
+  var task = runningJobs[taskPiece.taskId];
+  if (task !== undefined) {
+	var data = [];
+	for(var i = 0; i < taskPiece.data.length; i++) {
+		var item = taskPiece.data[i];
+    		var p = new Parallel(item, {
+			env: {
+				remote_code:task.code
+			}
+		});
+
+		p.spawn(function(data) {
+			console.log(data);
+			var fn = new Function("n", "return (" + global.env.remote_code + ")(n);");
+			return {pieceId: data.pieceId, value: fn(data.value)};
+		}).then(function(data) {
+			console.log(data);
+			socket.emit(task.ret, data, taskPiece.taskId);
+		});	
+	}
+  	} else {
+    		console.log("non-existing job for that piece. ignore");
+  	}
+
+});
+
+
+socket.on('finishJob', function (taskId)  {
+  delete runningJobs[taskId]
+});
+
+socket.on('disconnect', function(){
+  console.log("disconnect happened");
+
+});
